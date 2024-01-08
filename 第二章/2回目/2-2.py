c8b7748aa47a4fd03e125c6fd343bc8696fd48fd
tatuya06 <155950768+tatuya06@users.noreply.github.com>
2024-01-08 17:22:16 +0900

    Create 2-2.py

---
+++ 第二章/2回目/2-2.py
@@ -0,0 +1,18 @@
+import random
+
+level = 1
+
+def start_message():
+  print('違う漢字の番号(例:A1)を入力してください')
+
+def section_message():
+    print ('レベル：'+str(level))
+
+
+def play():
+    start_message()
+    section_message()
+    choice =  input('(例:A1)')
+    print('デバック：choice = '+choice)
+
+play()
